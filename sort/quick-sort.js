a87e159a3620d507892ccb989b3eb9833b728d13
Sumant Dey <sumantav19@yahoo.in>
2019-06-12 13:16:18 +0530

    fixed qick sort

--- sort/quick-sort.js
+++ sort/quick-sort.js
@@ -1,30 +1,8 @@
-var swap = require("./swap");
-var quickSort = module.exports = function (arr, low, high) {
-    if (arr.length == 0) {
-        return arr;
-    }
-    low = low || 0;
-    high = typeof high === "undefined" ?  arr.length - 1 : high;
-    if (low < high) {
-        pi = partition(arr, low, high)
-
-         quickSort(arr, low, pi - 1);
-         quickSort(arr, pi + 1, high);
-    }
-    return arr;
+let quickSort = module.exports = ([pivot,...rest])=>{
+  if(pivot){
+    return quickSort(rest.filter((curr)=> curr < pivot))
+    .concat(pivot)
+    .concat(quickSort(rest.filter((curr)=> curr >= pivot)))
+  }
+  return [];
 }
-var partition = function (arr, low, high) {
-    var pivot = arr[high];
-    var i = low-1;
-    for (var j = low; j < high; j++) {
-        if (arr[j] <= pivot) {
-            i++;
-            swap(arr, i, j);
-        }
-    }
-    swap(arr, i + 1, high);
-    //console.log(arr);
-    return (i + 1);
-}
-
-
